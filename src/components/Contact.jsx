9cbc97e2626d3195591a5f533bf5419e312727c3
Yada Martins Tisan <tisanyada@gmail.com>
2022-03-26 13:47:16 +0100

    added contact section -responsive

---
+++ src/components/Contact.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components"
+
+
+export const ContactContainer = styled.div`
+    width: 85%;
+    background-color: var(--orange);
+    margin: 100px auto 0;
+    text-align: center;
+    padding: 40px 200px;
+    
+    h4 {
+        font-size: 48px;
+        font-weight: 600;
+        color: var(--white);
+    }
+    p.lead {
+        color: var(--white);
+        margin: 20px auto;
+    }
+`
--- src/container/Contact/Contact.jsx
+++ src/container/Contact/Contact.jsx
@@ -1,9 +1,28 @@
+import { ContactContainer } from "@/components/Contact"
+import { InputGroup, SearchInput } from "@/components/Home"
+import { Button, Wrapper } from "@/components/Utils"
 
 
 const Contact = () => {
 	return (
-	  <div>Contact</div>
+		<ContactContainer id="contact">
+			<h4>Get your updated news</h4>
+			<p className="lead">If you are going to use a passage from Lorem Ipsum, you need to be sure there isn't anything embarrassing.</p>
+
+			<InputGroup>
+				<SearchInput type="email" placeholder="Enter your email" />
+				<Button
+					color="white"
+					bg="orange"
+					fw={600}
+					type={1}
+					shadow={2}
+					radius={50}
+					inputBtn={true}
+				>SEND</Button>
+			</InputGroup>
+		</ContactContainer>
 	)
-  }
-  
-  export default Contact
+}
+
+export default Contact
